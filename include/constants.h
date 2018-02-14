28b459047491f6099d2c9104d663f3e736f3859f
geniejohn <geniejohngaming@gmail.com>
2018-02-14 18:13:54 +0100

    Added: New const GHOST_AMOUNT in constants.h and additional Enum-value in constants.h.

--- include/constants.h
+++ include/constants.h
@@ -2,6 +2,7 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
 const int TEXTURE_COUNT = 1;		//The total number of textures that will be loaded.
+const int GHOST_AMOUNT = 4;
 const float HORIZONTAL_GAMESPACE = 0.7;			//Proportion of window containing the game.
 const float VERTICAL_GAMESPACE = 0.7;			//Proportion of window containing the game.
 
@@ -13,5 +14,5 @@ enum Texture
 
 enum Direction
 {
-	left, up, right, down,
+	left, up, right, down, still,
 };
