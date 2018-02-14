0b8065c468d902b9ba06637295fb5c1809efba3b
geniejohn <geniejohngaming@gmail.com>
2018-02-14 19:04:23 +0100

    Added: Constant float 'CENTER_TOLERANCE' variable to constants.h.

--- include/constants.h
+++ include/constants.h
@@ -1,10 +1,11 @@
 #pragma once
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
-const int TEXTURE_COUNT = 1;		//The total number of textures that will be loaded.
+const int TEXTURE_COUNT = 1;					//The total number of textures that will be loaded.
 const int GHOST_AMOUNT = 4;
-const float HORIZONTAL_GAMESPACE = 0.7;			//Proportion of window containing the game.
-const float VERTICAL_GAMESPACE = 0.7;			//Proportion of window containing the game.
+const float HORIZONTAL_GAMESPACE = 0.f;			//Proportion of window containing the game.
+const float VERTICAL_GAMESPACE = 0.7f;			//Proportion of window containing the game.
+const float CENTER_TOLERANCE = 1.0f;			//Tolereance for character snap to center.
 
 
 enum Texture
