9b23a5116a463851fa2e8c1cefcd44d0f09af3b3
geniejohn <geniejohngaming@gmail.com>
2018-02-15 00:02:59 +0100

    Added Enum 'Character' to constants.h.

--- toBeMade/pacman.hpp
+++ toBeMade/pacman.hpp
@@ -1,1 +1,22 @@
 #define "MovableCharacter"
+
+
+class Pacman : MovableCharacter
+{
+private:
+
+
+public:
+	//MovableCharacter(int Id, int x, int y)
+	Pacman(int Id) : public MovableCharacter(Id)				//Call base-class constructor:
+	{
+
+	}
+
+
+	~Pacman()
+	{
+
+	}
+
+};
