d0d92ca213bd6ac8c29d9f0024314ec87bd935d7
geniejohn <geniejohngaming@gmail.com>
2018-02-15 17:36:20 +0100

    Added: SCORE_PELLET to constants.h.

--- include/constants.h
+++ include/constants.h
@@ -4,13 +4,14 @@ const int WINDOW_HEIGHT = 800;
 const int TEXTURE_COUNT = 2;					//The total number of textures that will be loaded.
 const int GHOST_AMOUNT = 4;
 const int STARTING_LIVES = 3;
+const int SCORE_PELLET = 10;
+
 const float HORIZONTAL_GAMESPACE = 0.7f;		//Proportion of window containing the game.
 const float VERTICAL_GAMESPACE = 0.7f;			//Proportion of window containing the game.
 const float CENTER_TOLERANCE = 0.15f;			//Tolereance for character snap to center.
 //Speed in tiles per second.
 const float GHOST_SPEED = 2.8f;
 const float PACMAN_SPEED = 2.5f;
-
 
 enum Texture									//The different textrues a static sprite can have.
 {
