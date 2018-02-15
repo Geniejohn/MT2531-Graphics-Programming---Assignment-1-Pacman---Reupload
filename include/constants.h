f1aa8ff71c7a884ed25dbad05060cad049c30f70
geniejohn <geniejohngaming@gmail.com>
2018-02-15 12:23:28 +0100

    Added: STARTING_LIVES in constants.h

--- include/constants.h
+++ include/constants.h
@@ -3,6 +3,7 @@ const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
 const int TEXTURE_COUNT = 2;					//The total number of textures that will be loaded.
 const int GHOST_AMOUNT = 4;
+const int STARTING_LIVES = 3;
 const float HORIZONTAL_GAMESPACE = 0.7f;			//Proportion of window containing the game.
 const float VERTICAL_GAMESPACE = 0.7f;			//Proportion of window containing the game.
 const float CENTER_TOLERANCE = 0.25f;			//Tolereance for character snap to center.
