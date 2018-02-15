4e61deb7b1f001defce9086081d3957e4d159b06
JonShard <jonems@stud.ntnu.no>
2018-02-15 16:33:55 +0100

    Added: enum Animation that holds the different sprite animations.

--- include/constants.h
+++ include/constants.h
@@ -4,14 +4,19 @@ const int WINDOW_HEIGHT = 800;
 const int TEXTURE_COUNT = 2;					//The total number of textures that will be loaded.
 const int GHOST_AMOUNT = 4;
 const int STARTING_LIVES = 3;
-const float HORIZONTAL_GAMESPACE = 0.7f;			//Proportion of window containing the game.
+const float HORIZONTAL_GAMESPACE = 0.7f;		//Proportion of window containing the game.
 const float VERTICAL_GAMESPACE = 0.7f;			//Proportion of window containing the game.
 const float CENTER_TOLERANCE = 0.25f;			//Tolereance for character snap to center.
 
 
-enum Texture
+enum Texture									//The different textrues a static sprite can have.
 {
 	test, empty, pellet, upgrade,
+};
+
+enum Animation									//The different sprite sheet animations in the game.
+{
+	pacmanAnimation, ghostAnimation,
 };
 
 enum Character
@@ -22,9 +27,9 @@ enum Character
 enum Direction
 {
 	still, left, up, right, down,
-};
 
-const int STARTING_DIRECTION = right;							//Below 'Direction' definition.
+};
+const int STARTING_DIRECTION = right;			//Below 'Direction' definition.
 
 //Can not be const.
 char FILES[2][50] =								//List of paths to all the files used by the game.
