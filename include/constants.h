a20e7adfa12bf30e0df6a640e4d741d5ef25694f
JonShard <jonems@stud.ntnu.no>
2018-02-15 10:49:37 +0100

    Fixed: a syntax error I made while merging constants.h.

--- include/constants.h
+++ include/constants.h
@@ -2,8 +2,6 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
 const int TEXTURE_COUNT = 2;					//The total number of textures that will be loaded.
-const float HORIZONTAL_GAMESPACE = 0.7;			//Proportion of window containing the game.
-const float VERTICAL_GAMESPACE = 0.7;			//Proportion of window containing the game.
 const int GHOST_AMOUNT = 4;
 const float HORIZONTAL_GAMESPACE = 0.0f;			//Proportion of window containing the game.
 const float VERTICAL_GAMESPACE = 0.7f;			//Proportion of window containing the game.
