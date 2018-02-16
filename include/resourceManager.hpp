c1949aaa822f85e367a8e0b1f928399ca08c6a0d
JonShard <jonems@stud.ntnu.no>
2018-02-16 15:29:16 +0100

    Added: a test to ensure successful texture loading, comments.

--- include/resourceManager.hpp
+++ include/resourceManager.hpp
@@ -21,11 +21,11 @@ class ResourceManager
 		GLuint textures[TEXTURE_COUNT];										//Holds the textures for all possible sprites in the game.
 
 
-		void loadTextureFromFile(char* path, int no)
-		{
-			LOG_DEBUG("Loading texture: %d from file: %s", no, path);
+		int loadTextureFromFile(char* path, int no)								//Loads a texture form file into video memory.
+		{																		//gives error code if fails.
+			LOG_DEBUG("Attempting to load texture: %d from file: %s", no, path);
 			int width, height;
-			unsigned char* image;
+			unsigned char* image = nullptr;
 			float x, y, widthT, heightT;
 
 			// glActiveTexture(GL_TEXTURE0);
@@ -35,13 +35,23 @@ class ResourceManager
 			SOIL_free_image_data(image);
 
 
-			glUniform1i(glGetUniformLocation(shaderProgram, "texOne"), no);
-
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			glUniform1i(glGetUniformLocation(shaderProgram, "texOne"), 0);		//The last peramiter in the setting of the texture glUniform1i
+																				//is that index in the shader it is, and not which of the stored
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);//textures. So we can store 500 textures, but if we're only using
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);//one texture at a time in the shader, this will always be 0!!
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			LOG_DEBUG("Finished loading texture: %d from file: %s", no, path);
+
+			if(image == nullptr)
+			{
+				LOG_DEBUG("Failure!! %d from file: %s did not load.", no, path);
+				return -1;
+			}
+			else
+			{
+				LOG_DEBUG("Done.");
+				return 0;
+			}
 
 		}
 
@@ -56,9 +66,9 @@ class ResourceManager
 		}
 
 		//Setting up attributes for shaderProgram:
-		void loadShaderAttributes()
-		{
-			GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+		void loadShaderAttributes()												//Setup shader attribs, is called in setup, and in every sprite's setup.
+		{																		//binds the sprite's corrsponding VAO and VBO. Alot of debugging and trouble
+			GLint posAttrib = glGetAttribLocation(shaderProgram, "position");	//went in to finding out that this setup had do be done for every VAO-VBO-set.
 			glEnableVertexAttribArray(posAttrib);
 			glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), 0);
 
@@ -73,7 +83,7 @@ class ResourceManager
 
 		int startup()
 		{
-			if (!glfwInit())
+			if (!glfwInit())													//Start up glfw, will then open window of set size in constatns.h.
 			{
 				LOG_DEBUG("Failed to initialize GLFW\n");
 				getchar();
@@ -100,26 +110,26 @@ class ResourceManager
 
 			glewExperimental = GL_TRUE;
 			// Initialize GLEW
-			if (glewInit() != GLEW_OK) {
-				LOG_DEBUG("Failed to initialize GLEW\n");
+			if (glewInit() != GLEW_OK) {										//Initialize GLEW needs be happen before any other GL functions
+				LOG_DEBUG("Failed to initialize GLEW\n");						//can be called.
 				getchar();
 				glfwTerminate();
 				return -1;
 			}
 
 
-			glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+			glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);				//Input method, dictates how keypresses are interprited.
 			glfwSwapInterval(1);
 
 
 			glEnable(GL_BLEND);
 			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-			glClearColor(1,1,1,1);
+			glClearColor(1,1,1,1);												//The background color for the window.
 
 
 
-			shaderProgram = create_program("vertex.vert", "fragment.frag");
+			shaderProgram = create_program("vertex.vert", "fragment.frag");		//Create shaderProgram, attribs will be set at end of setup sequence.
 
 
 
@@ -127,10 +137,14 @@ class ResourceManager
 
 			for(int i = 0; i < TEXTURE_COUNT; i++)
 			{
-				loadTextureFromFile(FILES[i], i);
+				if(loadTextureFromFile(FILES[i], i) != 0)						//If a texture didn't load:
+				{
+					LOG_DEBUG("A texture number %d failed to load.", i);
+					return -1;													//stop, loading, return, then stop the program.
+				}
 			}
 
-			return 0;
+			return 0;															//Everyting is set up correctly.
 		}
 
 		void shutdown()
