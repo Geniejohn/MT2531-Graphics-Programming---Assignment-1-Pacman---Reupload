0965708a1bbba15f7aef57aad4b362d6f22fc8e1
JonShard <jonems@stud.ntnu.no>
2018-02-16 18:41:04 +0100

    Added: wall texture.

--- include/constants.h
+++ include/constants.h
@@ -6,16 +6,16 @@ const int STARTING_LIVES = 3;
 const int SCORE_PELLET = 10;
 
 const float HORIZONTAL_GAMESPACE = 0.7f;		//Proportion of window containing the game.
-const float VERTICAL_GAMESPACE = 0.7f;			//Proportion of window containing the game.
-const float CENTER_TOLERANCE = 0.15f;			//Tolereance for character snap to center.
+const float VERTICAL_GAMESPACE = 0.9f;			//Proportion of window containing the game.
+const float CENTER_TOLERANCE = 0.5f;			//Tolereance for character snap to center.
 //Speed in tiles per second.
 const float GHOST_SPEED = 2.8f;
-const float PACMAN_SPEED =0.002f; //2.5f;
+const float PACMAN_SPEED =0.001f; //2.5f;
 
-const int TEXTURE_COUNT = 2;					//The total number of textures that will be loaded.
-enum Texture : int									//The different textrues a static sprite can have.
+const int TEXTURE_COUNT = 4;					//The total number of textures that will be loaded.
+enum Texture : int								//The different textrues a static sprite can have.
 {
- 	pellet, empty, pacSheet, ghostSheet, upgrade, wall,
+ 	pellet, wall, empty, test, pacSheet, ghostSheet, upgrade,
 };
 
 enum Animation									//The different sprite sheet animations in the game.
@@ -36,11 +36,12 @@ enum Direction
 const int STARTING_DIRECTION = right;			//Below 'Direction' definition.
 
 //Can not be const:
-char FILES[5][50] =								//List of paths to all the files used by the game.
+char FILES[6][50] =								//List of paths to all the files used by the game.
 {												//should be automated to read from folder instead in the future.
 	"./assets/pellet.png",
+	"./assets/wall.png",
 	"./assets/empty.png",
+	"./assets/test.png",
 	"./assets/pacman.png",
 	"./assets/ghost.png",
-	"./assets/test.png"
 };
