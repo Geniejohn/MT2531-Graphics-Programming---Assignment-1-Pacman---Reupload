fead8c1e3876d5ecc6214f5ac38dc17b1ff529a6
JonShard <jonems@stud.ntnu.no>
2018-02-18 00:01:55 +0100

    Fixed: the score now stays centered with varying amount of digits.

--- include/gameUI.hpp
+++ include/gameUI.hpp
@@ -11,14 +11,15 @@ class GameUI
 	private:
 
 		Counter scoreCounter;
-		//Sprite lives[MAX_LIVES];
+		Sprite lives[MAX_LIVES];
 		//Cherry[] not implemented.
 
 	public:
 
 		void startup()
 		{
-			scoreCounter = Counter(glm::vec2(0, 0.85f), glm::vec2(0.08f,0.08f));
+			scoreCounter = Counter(glm::vec2(SCORE_DIGITS * -0.031f, 0.88f), glm::vec2(0.085f,0.085f));
+
 		}
 
 		void increaseScore(int increase)
@@ -30,7 +31,12 @@ class GameUI
 		void draw()
 		{
 			scoreCounter.draw();
-			//Draw lives
+
+			for(int i = 0; i < MAX_LIVES; i++)	//Draw all lives icons:
+			{
+
+			}
+
 			//Draw cherrys
 		}
 };
