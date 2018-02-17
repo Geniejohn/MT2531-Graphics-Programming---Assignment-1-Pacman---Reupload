e64c8a84b71bb7b9ad25d3719f4a78f0ebcf61ab
JonShard <jonems@stud.ntnu.no>
2018-02-17 21:53:52 +0100

    Refactored: Main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,41 +10,42 @@
 #include "ghost.hpp"
 #include <vector>
 
-ResourceManager resourceManager;
-InputManager inputManager;
-GameLoop gameLoop;
-Pacman player;
-std::vector<Ghost> ghosts;										//Vector to hold ghosts.
+ResourceManager resourceManager;		//Loads and constains all texturs as well as handeling most of the OpenGL calls.
+InputManager inputManager;				//Sends signals to pacman, UI, and gameloop depending input from user.
+GameLoop gameLoop;						//The object that handles everyhing having to do with running the game itself.
+Pacman player;								//The player object. Is global since so many systems is dependent on information from pacman.
+std::vector<Ghost> ghosts;				//Vector to hold ghosts.
 
 
 Level level;
-float dt;																		//DeltaTime: the time it took to complete the last frame.
+float dt;																			//DeltaTime: the time it took to complete the last frame.
 
 int main()
 {
-	if(resourceManager.startup() != 0)											//Failed to load any of the textures:
+	if(resourceManager.startup() != 0)										//Failed to load any of the textures:
 	{
 		LOG_DEBUG("Failed to run resourceManager startup.");
-		return -1;																//Exit with code -1.
+		return -1;																	//Exit with code -1.
 	}
 
-	level = Level(0);
-	gameLoop.makeSprites();
+	gameLoop.makeSprites(0);													//Parameter is what level to load.
 	LOG_DEBUG("Level created");
-	resourceManager.loadShaderAttributes();										//VERY IMPORTANT, NEEDS TO BE THE LAST THING IN SETUP.
+	resourceManager.loadShaderAttributes();								//VERY IMPORTANT, NEEDS TO BE THE LAST THING IN SETUP.
 
 
 	int nbFrames = 0;
 	double currentTime = 0;
 	double lastTime = glfwGetTime();
 
-	while(true)//MainMenu() returns true. Meaning, we are running another game.
+	while(true)																		//MainMenu() returns true. Meaning, we are running another game.
 	{
+																						//We now entered a game.
 	    do
-	    {																			//Measure speed:
+	    {																				//Keep running game.
+			 																			//Measure speed:
 			currentTime = glfwGetTime();
 			nbFrames++;
-			if (currentTime - lastTime >= 1.0) { 								// If last prinf() was more than 1sec ago
+			if (currentTime - lastTime >= 1.0) { 							// If last prinf() was more than 1sec ago
 				dt = 1.0 / double(nbFrames);									// printf and reset
 				printf("%f dt\n", dt);
 				nbFrames = 0;
@@ -52,10 +53,13 @@ int main()
 			}
 			gameLoop.run();														//The gamloop updates and draws everything game realated.
 
-	    } while(gameLoop.getRunning());
+		} while(gameLoop.getRunning());										//Exit out of game if press esc or collect all pellets.
+
+		 //Show game over menu().
+
 	}
 
-	resourceManager.shutdown();
+	resourceManager.shutdown();												//Time to close the window.
 
     return (0);
 }
