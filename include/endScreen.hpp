81152cb4cb995ccff75311b4b495d8625076a372
geniejohn <geniejohngaming@gmail.com>
2018-02-17 22:30:39 +0100

    Added: endScreen.hpp - just a skeleton.

---
+++ include/endScreen.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "logger.h"
+#include "glm/glm/glm.hpp"
+#include "sprite.hpp"
+#include "constants.h"
+#include "counter.hpp"
+
+class EndScreen
+{
+	private:
+		Sprite score;
+		Sprite time;
+		Sprite gameOverSprite;
+		Sprite victorySprite;
+		Sprite pressAny;						//'Press any key to return to main menu'.
+		Counter scoreCounter;
+
+
+		//Takes ints and returns position.
+		//E.G. fracX = 
+		glm::vec2 retWPos(int fracX, int fracY)
+		{
+
+		}
+
+	public:
+
+		void startup()
+		{
+			gameOverSprite = new Sprite();
+			scoreCounter = Counter(glm::vec2(0, 0.7f), glm::vec2(33,30));
+		}
+
+
+		void draw()
+		{
+			scoreCounter.draw();
+			//Draw lives
+			//Draw cherrys
+		}
+};
